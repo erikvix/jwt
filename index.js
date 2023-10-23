2a0e068021b1a53edadc9c0142c249d61080cead
Erik Nunes <erikvixx@gmail.com>
2023-10-23 10:38:35 -0300

    feat; include accessToken on post

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
-app = express();
+const app = express();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
+
+require('dotenv').config();
+app.use(express.json());
+
+const users = [{
+    name: 'Gust',
+    pass: 'gust12'
+},
+{
+    name: 'Dan',
+    pass: 'dan897'
+}]
+
+app.get('/', (req, res) =>{
+res.json(users)
+})
+
+app.post('/', (req, res) =>{
+    let username = req.body.name;
+    let user = {name : username}
+    
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
+    res.json({ accessToken: accessToken})
+})
 
 
 app.listen(port, () =>{
