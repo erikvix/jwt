0dfb8063cd0276bdbd7fbde41d1950eb2c3b00de
Erik Nunes <erikvixx@gmail.com>
2023-10-23 11:57:29 -0300

    feat: include authToken function

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const users = [{
 }]
 
 app.get('/', (req, res) =>{
-res.json(users)
+res.json(users.filter(user => user.name === req.user.name))
 })
 
 app.post('/', (req, res) =>{
@@ -26,6 +26,21 @@ app.post('/', (req, res) =>{
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
     res.json({ accessToken: accessToken})
 })
+
+const authToken = (req, res, next) =>{
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null){
+        return res.sendStatus(401)
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) =>{
+        if(err){
+            return res.sendStatus(403)
+        }
+        req.user = user
+        next();
+    })
+}
 
 
 app.listen(port, () =>{
